2aa0776d0ce9dff8d52d72c2aac8690591a6c66d
yogitagupta27 <43501364+yogitagupta27@users.noreply.github.com>
2020-04-03 18:46:31 +0530

    Update App.java

--- src/main/java/com/javainterviewpoint/my_helloworld/App.java
+++ src/main/java/com/javainterviewpoint/my_helloworld/App.java
@@ -1,23 +1,23 @@
 package com.javainterviewpoint.my_helloworld;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
-/**
- * Hello world!
- *
- */
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 @EnableAutoConfiguration
-
+@EnableWebSecurity
+@SpringBootApplication
 public class App 
 {
-	@RequestMapping("/")
+	@GetMapping("/")
     String hello() {
-        return "Hello World!";
+    
+    return " ENTER CORRECT USERNAME AND PASSWORD TO LOGIN....!";
+    
     }
 	
 	public static void main(String[] args) throws Exception {
